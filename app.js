c00d4af37b75c58402782c3561c2718e8fdccc58
Andrew Marchukov <55357213+fokk1@users.noreply.github.com>
2019-12-21 22:22:43 +0300

    Added cursor movement

--- app.js
+++ app.js
@@ -1,62 +1,69 @@
 function isTouching(a, b) {
-  const aRect = a.getBoundingClientRect();
-  const bRect = b.getBoundingClientRect();
+    const aRect = a.getBoundingClientRect();
+    const bRect = b.getBoundingClientRect();
 
-  return !(
-    aRect.top + aRect.height < bRect.top ||
-    aRect.top > bRect.top + bRect.height ||
-    aRect.left + aRect.width < bRect.left ||
-    aRect.left > bRect.left + bRect.width
-  );
+    return !(
+        aRect.top + aRect.height < bRect.top ||
+        aRect.top > bRect.top + bRect.height ||
+        aRect.left + aRect.width < bRect.left ||
+        aRect.left > bRect.left + bRect.width
+    );
 }
 
 const player = document.querySelector('#player');
 const coin = document.querySelector('#coin');
 const score = document.querySelector('.score');
+const boxElem = document.querySelector('#box');
 
-window.addEventListener('keydown', function(key) {
-  if (key.key === 'ArrowUp') {
-    const currVertical = extractPos(player.style.top);
-    player.style.top = `${currVertical - 50}px`;
-  }
-  else if (key.key === 'ArrowDown') {
-    const currVertical = extractPos(player.style.top);
-    player.style.top = `${currVertical + 50}px`;
-  }
+//mouse watching
 
-  else if (key.key === 'ArrowLeft') {
-    const currHorizontal = extractPos(player.style.left);
-    player.style.transform = 'scale(-1,1)';
-    player.style.left = `${currHorizontal - 50}px`;
-  }
-  else if (key.key === 'ArrowRight') {
-    const currHorizontal = extractPos(player.style.left);
-    player.style.transform = 'scale(1,1)';
-    player.style.left = `${currHorizontal + 50}px`;
-  }
+window.onload = () => {
+    function onMouseMove(event) {
+        const mouseX = event.pageX;
+        const mouseY = event.pageY;
+        const crd = boxElem.getBoundingClientRect();
+        const activePointer = crd.left <= mouseX && mouseX <= crd.right - 150 && crd.top <= mouseY && mouseY <= crd.bottom - 150;
+        requestAnimationFrame(function movePointer() {
+            if (activePointer) {
+                player.classList.remove('hidden');
+                player.style.left = Math.floor(mouseX) + 'px';
+                player.style.top = Math.floor(mouseY) + 'px';
+            } else {
+                player.classList.add('hidden');
+            }
+            if (isTouching(player, coin)) {
+                coinPos();
+                counter();
+            };
+        });
+    }
 
-  if(isTouching(player, coin)) {
+    function disablePointer() {
+        requestAnimationFrame(function hidePointer() {
+            player.classList.add('hidden');
+        });
+    }
+
+    boxElem.addEventListener('mousemove', onMouseMove, false);
+    boxElem.addEventListener('mouseleave', disablePointer, false);
+
+    const extractPos = (pos) => {
+        if (!pos) return 10;
+        return parseInt(pos.slice(0, -2));
+    }
+
+    const coinPos = () => {
+        const width = Math.floor(Math.random() * (window.innerWidth - 100));
+        const height = Math.floor(Math.random() * (window.innerHeight - 100));
+        coin.style.top = `${height}px`;
+        coin.style.left = `${width}px`;
+    }
+
+    function counter() {
+        let num = parseInt(score.textContent);
+        num += 1;
+        score.textContent = num;
+    }
+
     coinPos();
-    counter();
-  };
-});
-
-const extractPos = (pos) => {
-  if(!pos) return 10;
-  return parseInt(pos.slice(0, -2));
 }
-
-const coinPos = () => {
-  const width = Math.floor(Math.random() * (window.innerWidth - 100));
-  const height = Math.floor(Math.random() * (window.innerHeight - 100));
-  coin.style.top = `${height}px`;
-  coin.style.left = `${width}px`;
-}
-
-function counter() {
-	let num = parseInt(score.textContent);
-	num += 1;
-	score.textContent = num;
-}
-
-coinPos();
